c5bf3cb626e12f5f3a4490ba6c75152cbd1eff0b
CaChiJ <chj7239@gmail.com>
2025-01-22 01:24:19 +0900

    chore: add speed insight

--- lightning-web/app/layout.tsx
+++ lightning-web/app/layout.tsx
@@ -1,18 +1,20 @@
 import "./globals.css";
 import { Providers } from "@/app/providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return ( 
-      <html lang="en">
-        <body
-          className={`antialiased`}
-        >
-          <Providers>{children}</Providers>
-        </body>
-      </html>
+  return (
+        <html lang="en">
+          <body
+            className={`antialiased`}
+          >
+            <SpeedInsights />
+            <Providers>{children}</Providers>
+          </body>
+        </html>
   );
 }
