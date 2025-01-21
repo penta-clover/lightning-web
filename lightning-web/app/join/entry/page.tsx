497bc001fdb9b8ace31aad69af8b6c0067d13721
CaChiJ <chj7239@gmail.com>
2025-01-22 00:11:38 +0900

    fix: undefined 오류 해결

--- lightning-web/app/join/entry/page.tsx
+++ lightning-web/app/join/entry/page.tsx
@@ -13,15 +13,15 @@ export default function Page() {
             return;
         } else if (status === "unauthenticated") {
             router.push('/');
-        } else if (session.memberId) {
+        } else if (session!.memberId) {
             // 사용자 정보가 있다면, 회원가입을 수행하지 않고 메인 페이지로 이동
             router.push('/');
         } else {
             // 사용자 정보가 없다면, 회원가입 페이지로 이동
             const params = new URLSearchParams()
-            params.append("socialType", session.socialType);
-            params.append("socialId", session.socialId);
-            params.append("email", session.user?.email ?? "");
+            params.append("socialType", session!.socialType!);
+            params.append("socialId", session!.socialId!);
+            params.append("email", session!.user?.email ?? "");
 
             router.push(`/join/step1?${params.toString()}`);
         }
