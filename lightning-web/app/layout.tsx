e8df9eac767c7c699c2f123df4caeca15e3dd858
CaChiJ <chj7239@gmail.com>
2025-01-28 02:00:57 +0900

    ui: 사파리 주소창 대응

--- lightning-web/app/layout.tsx
+++ lightning-web/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
         <html lang="en">
           <body
-            className={`antialiased overflow-hidden`}
+            className={`antialiased overflow-hidden safe-area`}
           >
             <SpeedInsights />
             <LayoutUI>
