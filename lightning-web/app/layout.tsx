c812b31b314a09a333a0e49711fcbf7e77e5d133
CaChiJ <chj7239@gmail.com>
2025-01-31 11:45:09 +0900

    favicon 로고 변경

--- lightning-web/app/layout.tsx
+++ lightning-web/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <link rel="icon" href="/favicon.svg" />
       </head>
       <body className={`antialiased overflow-hidden safe-area`}>
         <SpeedInsights />
