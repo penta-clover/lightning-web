464828fe93e26c7dec458dbb10ac4b1e2e47b29c
CaChiJ <chj7239@gmail.com>
2025-02-05 14:53:50 +0900

    ui: 캐치 프레이즈 변경

--- lightning-web/app/page.tsx
+++ lightning-web/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     return (
       <div className="flex h-full flex-col justify-center">
         <div className="flex flex-row justify-center font-semibold font-[Pretendard]">
-          비난 조롱 없는 클린 스포츠챗
+          스트레스 없는 새로운 LCK의 시작
         </div>
 
         <div className="flex flex-col items-center mx-[17%] sm:mx-[30%] mb-[33px] mt-[26px]">
