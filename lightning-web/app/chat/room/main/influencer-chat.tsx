e9c31735a1b48cb4fb3e904a2f7bd43d1bfb1ec9
CaChiJ <chj7239@gmail.com>
2025-02-06 17:20:32 +0900

    ui: add influencer profile style

--- lightning-web/app/chat/room/main/influencer-chat.tsx
+++ lightning-web/app/chat/room/main/influencer-chat.tsx
@@ -5,7 +5,7 @@ export default function InfluencerChat(props: {
   onClickLightning: (chatId: string) => void;
   onClickLink: () => void;
 }) {
-  const {chat, onClickLightning, onClickLink} = props;
+  const { chat, onClickLightning, onClickLink } = props;
 
   return (
     <>
@@ -15,13 +15,26 @@ export default function InfluencerChat(props: {
         src={chat.profile_image_url}
         alt={`${chat.sender_nickname} 프로필`}
         className="w-10 h-10 rounded-[12] mr-[16px]"
+        onClick={onClickLink}
       />
       <div className="flex flex-col">
         <div className="flex items-end">
           <div className="flex flex-col">
             {/* 닉네임 */}
-            <div className="text-caption12 text-darkgray mb-[7px]">
-              {chat.sender_nickname}
+            <div className="flex flex-row justify-start items-center mb-[7px] ">
+              <div
+                className="text-caption12 text-blue font-medium"
+                onClick={onClickLink}
+              >
+                {chat.sender_nickname}
+              </div>
+              <Image
+                src="/icon/official_mark.svg"
+                width={10}
+                height={10}
+                alt="official"
+                className="ml-[2px]"
+              />
             </div>
             {/* 메시지 내용 */}
             <div className="text-black text-body14 font-medium px-[12px] py-[8px] bg-bgblue rounded-[4px] break-all max-w-xs">
@@ -46,11 +59,14 @@ export default function InfluencerChat(props: {
             }
           />
         </div>
-        <div className="m-[3px] text-caption12 text-darkgray" onClick={onClickLink}>
+        <div
+          className="m-[3px] text-caption12 text-darkgray"
+          onClick={onClickLink}
+        >
           <div>{chat.optional!.introduction_on_chat ?? ""}</div>
           <div className="underline">{chat.optional!.cta_on_chat ?? ""}</div>
         </div>
       </div>
     </>
   );
-};
+}
